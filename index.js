c6aa835af724d1e7a049eafb5709a895156b7bd8
loribao <lorival.sanjinez@hotmail.com>
2021-03-07 23:07:45 -0300

    correção na logica de validação

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const main = () => {
     let querFazerOutroCalculo = false;
     let eficiencia = 0.7;
     let calcular = (alcool, gasolina) => {
-        let relacao = alcool / gasolina;
+        let relacao = parseFloat(alcool)/parseFloat(gasolina);
         if (relacao > eficiencia) {
             return "Vale a pena a gasolina."
         }
@@ -11,20 +11,20 @@ const main = () => {
         }
         return "vale a pena o álcoo"
     }
-    let validaEntrada = (entrada) => typeof (entrada) === 'boolean' || typeof (entrada) === 'number' ? true : false
+    let validaEntrada = (entrada) => !isNaN(parseFloat(entrada))  || typeof(entrada)=='boolean' ? true : false
     try {
         do {
             let lerDoNavegadorViaPrompt = prompt//arrumando a nomenclatura
-            let precoAlcool = lerDoNavegadorViaPrompt("Qual é o valor do alcool?");
+            let precoAlcool = lerDoNavegadorViaPrompt("Qual é o valor do alcool? ");
             if (!validaEntrada(precoAlcool)) {
                 return;
             }
-            let precoGasolina = lerDoNavegadorViaPrompt("Qual é o valor da gasolina?");
+            let precoGasolina = lerDoNavegadorViaPrompt("Qual é o valor da gasolina? ");
             if (!validaEntrada(precoGasolina)) {
                 return;
             }
             alert(calcular(precoAlcool, precoGasolina))
-            querFazerOutroCalculo = window.confirm('Fazer outro calculo?');
+            querFazerOutroCalculo = window.confirm('Fazer outro calculo? ');
             if (!validaEntrada(querFazerOutroCalculo)) {
                 return;
             }
@@ -33,7 +33,7 @@ const main = () => {
         const readline = require("readline");
         const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
         const lerDoNodeJs = () => {
-            rl.question("Qual é o valor do alcool?", function (alcool) {
+            rl.question("Qual é o valor do alcool? ", function (alcool) {
                 rl.question("Qual é o valor da gasolina? ", function (gasolina) {
                     validaEntrada(alcool) && validaEntrada(gasolina) ? console.log(calcular(alcool, gasolina)) : console.log('Apenas valores decimais ou inteiros. Ex: 10.50');
                     rl.question("Digite 's' para repetir ou qualquer tecla para sair ", function (_querFazerOutroCalculo) {
